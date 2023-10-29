148e6d4cbcd8ab31c0fea66410973408b51ce57d
muhammed-sayeed <sayeedmon25@gmail.com>
2023-10-29 20:44:25 +0530

    some unwanted codes and comments are removed

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,21 +1,18 @@
 import { check } from "express-validator";
 
 export const loginValidator = [
-    // check('username','Invalid username').isLength({min: 5}),
-    // check('email', 'Invalid email').isEmail(),
-    // check('password', 'The minimum password length is 6 characters').isLength({min: 6}),
-    check("username")
+  check("username")
     .isLength({ min: 5 })
-  .withMessage("is invalid, must contain minimum of 5 letters")
-  .isLength({ max: 15 })
-  .withMessage("is invalid, must contain maximum of 15 letters")
-  .trim(),
+    .withMessage("is invalid, must contain minimum of 5 letters")
+    .isLength({ max: 15 })
+    .withMessage("is invalid, must contain maximum of 15 letters")
+    .trim(),
   check("email")
-  .isEmail()
-  .normalizeEmail()
-  .withMessage("is invalid, must be a valid email!")
-  .trim(),
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("is invalid, must be a valid email!")
+    .trim(),
   check("password")
-  .isLength({ min: 5})
-  .withMessage('password must be 5 charectere')
-  ]
+    .isLength({ min: 5 })
+    .withMessage("password must be 5 charectere"),
+];
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,12 @@ import roleBasedAuthentication from '../middlewares/authenticate.js'
 
 const userRoute = express.Router()
 
+import {register, login} from '../controller/userController/registerController.js'
+import { fetchProducts } from '../controller/userController/productController.js'
+
 userRoute.post('/register',loginValidator,register)
 userRoute.post('/login',login)
 userRoute.get('/products',roleBasedAuthentication('user'),fetchProducts)
 
 export default userRoute
 
-import {register} from '../controller/userController/registerController.js'
-import { login } from '../controller/userController/loginController.js'
-import { fetchProducts } from '../controller/userController/productController.js'
